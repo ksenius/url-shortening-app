d9e538494cce3ecfb26e348b4ae7c0bc4c408de9
ksenia <30226190+ksenius@users.noreply.github.com>
2020-04-02 16:58:44 +0700

    Create container component

---
+++ src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+};
+</script>
+
+<style lang="scss">
+.container {
+  width: calc(100% - 5rem);
+  max-width: 111rem;
+  margin: 0 auto;
+}
+</style>
