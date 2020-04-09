55f1997242dc5a90cfd6baf2539d5cf3ed462fca
ksenia <30226190+ksenius@users.noreply.github.com>
2020-04-09 17:12:48 +0700

    Update logo, create nav list component

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="logo">
     <a href="/" title="Shortly - URL shortening app">
-      <img class="logo__img" :src="image" alt="Shortly logo" />
+      <img
+        v-if="isWhite"
+        class="logo__img"
+        :src="whiteImage"
+        alt="Shortly logo"
+      />
+      <img v-else class="logo__img" :src="image" alt="Shortly logo" />
     </a>
   </div>
 </template>
 
 <script>
 import image from '@images/logo.svg';
+import whiteImage from '@images/logo-white.svg';
 
 export default {
   name: 'Logo',
+  props: {
+    isWhite: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       image,
+      whiteImage,
     };
   },
 };
---
+++ src/components/NavList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="nav-list">
+    <Heading type="h4" class="nav-list__heading" :text="heading" />
+    <ul class="nav-list__items">
+      <li class="nav-list__item" v-for="(item, index) in items" :key="index">
+        <Link class="nav-list__link" :href="item.href" :text="item.text" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Heading from './Heading';
+import Link from './Link';
+
+export default {
+  name: 'NavList',
+  components: {
+    Heading,
+    Link,
+  },
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.nav-list {
+  font-size: 1.6rem;
+
+  &__heading {
+    color: #fff;
+    margin-bottom: 2.2rem;
+  }
+
+  &__items {
+    @include list-reset;
+  }
+
+  &__item {
+    &:not(:last-child) {
+      margin-bottom: 0.6rem;
+    }
+  }
+
+  &__link {
+    color: inherit;
+
+    &:hover {
+      color: $cyan;
+    }
+  }
+}
+</style>
